126ae98cce683c67d9a89ff33301e96ea5049809
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-24 15:31:42 +0300

    created a function that maps the results

--- Assets/index.js
+++ Assets/index.js
@@ -16,5 +16,39 @@ async function fetchAPI (){
     const baseUrl =`https://api.edamam.com/search?q=pizza&app_id=${APP_Id}&app_key=${APP_key}&to=30`; 
     const response = await fetch(baseUrl);
     const data = await response.json();
+    generateHTML(data.hits)
     console.log(data);
+}
+function generateHTML(results){
+    let generatedHTML = '';
+    results.map(result =>{
+        generatedHTML +=
+        `
+         <div class="item">
+        <img
+          src="${result.recipe.image}"
+          alt=""
+        />
+        <div class="flex-cont">
+          <h1 class="title">Check the recipe</h1>
+          <a
+            class="btn"
+            style="
+              text-decoration: none;
+              text-align: center;
+              width: 130px;
+              background-color: brown;
+              padding: 10px 0;
+              color: aliceblue;
+              font-size: 1.5rem;
+              margin-top: 20px;
+              align-self: flex-start;"
+            
+            href="#">View Recipe</a>
+          
+        </div>  
+        `
+
+    })
+    searchResultDiv.innerHTML
 }
