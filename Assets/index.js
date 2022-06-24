173eaa52c2f09f6816b0b1741842d49b55b188a4
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-24 15:03:21 +0300

    used the fetch method to get data from a public API

--- Assets/index.js
+++ Assets/index.js
@@ -2,9 +2,19 @@ const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
+const APP_Id = '07e48c42';
+const APP_key = '32d915ce0f9751188c2d9e17b8bc41de'
+const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${APP_Id}&app_key=${APP_key}`;
 
 searchForm.addEventListener('submit', (e) => {
 e.preventDefault();
 searchQuery = e.target.querySelector('input').value;
-console.log(searchQuery);
-})
+fetchAPI();
+});
+
+async function fetchAPI (){
+    const baseUrl =`https://api.edamam.com/search?q=pizza&app_id=${APP_Id}&app_key=${APP_key}`; 
+    const response = await fetch(baseUrl);
+    
+    console.log(response);
+}
