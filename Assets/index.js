a3f8afab9f57e7aa984f9a0c7019f2038ec7704e
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-26 08:40:42 +0300

    added a DOMContentLoaded event fires when the initial HTML document has been completely loaded

--- Assets/index.js
+++ Assets/index.js
@@ -1,11 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+});
 
+//fetching data from a public external API using app id and app key
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_Id = '07e48c42';
 const APP_key = '32d915ce0f9751188c2d9e17b8bc41de'
-const baseUrl = `https://api.edamam.com/search?q=pizza&app_id=${APP_Id}&app_key=${APP_key}`;
+const baseUrl = `https://api.edamam.com/search?q=chicken&app_id=${APP_Id}&app_key=${APP_key}`;
 
 searchForm.addEventListener('submit', (e) => {
 e.preventDefault();
@@ -13,6 +17,7 @@ searchQuery = e.target.querySelector('input').value;
 fetchAPI();
 });
 
+//API to fetch different recipes
 async function fetchAPI (){
     const baseUrl =`https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_Id}&app_key=${APP_key}&to=18`; 
     const response = await fetch(baseUrl);
